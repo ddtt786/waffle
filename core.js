da98eec80db20b1d7d52486ff63a1646c6b35095
ddtt786 <ddtt786@outlook.kr>
2023-05-01 16:08:06 +0900

    only one

--- core.js
+++ core.js
@@ -5,42 +5,44 @@ function range(d) {
   chrome.storage.local.get("block", function ({ block }) {
     d.forEach((dom) => {
       const contents = dom.querySelectorAll("div > div")[1];
-      [...contents.querySelectorAll("a")].slice(0, 2).forEach((link) => {
-        const url = link.href;
-        if (url.startsWith("https://playentry.org/uploads")) {
-          const user = link.parentElement.parentElement
-            .querySelector("li > div > a")
-            .href.match(/[a-f\d]{24}/)[0];
-          const blocked = block.includes(user);
+      const link = contents.querySelector("a");
+      if (!link) return;
+      const url = link.href;
+      if (url.startsWith("https://playentry.org/uploads")) {
+        const user = link.parentElement.parentElement
+          .querySelector("li > div > a")
+          .href.match(/[a-f\d]{24}/)[0];
+        let blocked = block.includes(user);
 
-          const image = document.createElement("img");
-          if (!blocked) {
-            image.src = url;
-          }
-          image.alt = "이 사용자는 차단되었습니다. 차단 해제하려면 클릭하세요.";
-          image.className = "waffle";
-          image.addEventListener("click", () => {
-            if (blocked) {
-              if (confirm("이 사용자를 차단 해제할까요?")) {
-                block = block.filter((u) => u !== user);
-                chrome.storage.local.set({ block });
-                image.src = url;
-              }
-            } else {
-              if (confirm("이 사용자를 차단할까요?")) {
-                block.push(user);
-                chrome.storage.local.set({ block });
-                image.removeAttribute("src");
-              }
-            }
-          });
-
-          link.innerText = null;
-          link.removeAttribute("href");
-          link.append(image);
-          link.style.display = "flex";
+        const image = document.createElement("img");
+        if (!blocked) {
+          image.src = url;
         }
-      });
+        image.alt = "이 사용자는 차단되었습니다. 차단 해제하려면 클릭하세요.";
+        image.className = "waffle";
+        image.addEventListener("click", () => {
+          if (blocked) {
+            if (confirm("이 사용자를 차단 해제할까요?")) {
+              block = block.filter((u) => u !== user);
+              chrome.storage.local.set({ block });
+              image.src = url;
+              blocked = false;
+            }
+          } else {
+            if (confirm("이 사용자를 차단할까요?")) {
+              block.push(user);
+              chrome.storage.local.set({ block });
+              image.removeAttribute("src");
+              blocked = true;
+            }
+          }
+        });
+
+        link.innerText = null;
+        link.removeAttribute("href");
+        link.append(image);
+        link.style.display = "flex";
+      }
     });
   });
 }
